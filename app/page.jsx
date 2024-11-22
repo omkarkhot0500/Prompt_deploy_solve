a63841845e3178c0d7d301d541da6be8792f3bb1
omkarkhot0500 <omkarkhot0500@gmail.com>
2024-11-22 22:35:02 +0530

    Undoing hack

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
 import Feed from "@components/Feed";
 
 const Home = () => (
-  <section className='w-full flex-center flex-col'>
-    <h1 className='head_text text-center'>
-    Unlock Your Academic Potential
-      <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+  <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">
+      Discover & Share
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center"> AI-Powered Prompts</span>
     </h1>
-    <p className='desc text-center'>
-    Save, Share, and Explore AI-Powered Prompts Tailored for Students – Your Shortcut to Smarter Learning and Creative Success
+    <p className="desc text-center">
+      PromptVerse is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
     </p>
 
     <Feed />
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -21,14 +21,14 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        {/* <Image
+        <Image
           src="/assets/images/logo.svg"
           alt="logo"
           width={30}
           height={30}
           className="object-contain"
-        /> */}
-        <p className="logo_text" style={{ fontSize: '30px' }}>StudyCraft</p>
+        />
+        <p className="logo_text" style={{ fontSize: '30px' }}>PromptVerse</p>
       </Link>
 
       {/* Desktop Navigation */}
